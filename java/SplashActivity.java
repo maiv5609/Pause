ffd39012ecc52683df4df088eb6fec9cdd3fb5fa
turner36 <summerkt99@gmail.com>
2018-02-24 15:52:10 -0800

    display splash screen

--- java/SplashActivity.java
+++ java/SplashActivity.java
@@ -1,6 +1,5 @@
 package com.pause;
 
-
 /**
  * Created by maiv on 2/12/18.
  */
@@ -9,14 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, getting_started.class);
-        startActivity(intent);
-        finish();
+
+        /**
+         * Delay 1000 ms to display splash activity and forward along to main activity.
+         */
+        Handler handle=new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },1000);
+
     }
 }
