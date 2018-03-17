ff77d73aeb878345c82c5f30860ecf502c4901a3
maiv <maiv@wwu.edu>
2018-03-16 19:58:51 -0700

    Got example graph working, uses library that i've added. Comment it out if it causes issues with your build

--- Pause/app/src/main/java/com/pause/StatsActivity.java
+++ Pause/app/src/main/java/com/pause/StatsActivity.java
@@ -1,9 +1,15 @@
 package com.pause;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
 
 /**
  * Created by turner36 on 2/27/18.
@@ -20,7 +26,30 @@ public class StatsActivity extends AppCompatActivity {
 
         homeButton = (ImageButton)findViewById(R.id.statsHomeButton);
         homeButton.setOnClickListener(selectHome);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, -1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+        // styling
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+
+        series.setSpacing(50);
+
+        // draw values on top
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
     }
+
 
     View.OnClickListener selectHome = new View.OnClickListener() {
         @Override
